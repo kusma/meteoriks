3f42de79393c84b6874f2b17d50290609e154fa8
Erik Faye-Lund <erik@fusetools.com>
2018-02-23 09:50:53 +0100

    add missing script

---
+++ _scripts/sort-prod-list.py
@@ -0,0 +1,17 @@
+import argparse
+import html
+import json
+import urllib.request
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", type=str,
+                    help="File to sort")
+args = parser.parse_args()
+
+with open(args.filename, encoding='utf8') as infile:
+    data = json.load(infile)
+
+data = sorted(data, key=lambda prod: int(prod["rank"]))
+
+with open(args.filename, 'w', encoding='utf8') as outfile:
+    json.dump(list(data), outfile, ensure_ascii=False, indent=4, sort_keys=True)
